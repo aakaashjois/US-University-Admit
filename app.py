6a964849c9567b252740012b5f051ab60e65751e
Aakaash Jois <aakaashjois@gmail.com>
2019-03-21 16:45:16 -0400

    Change code to get the port from Heroku

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os
 app = Flask(__name__)
 CORS(app)
 model = load(open('./models/rf_v1.pkl', 'rb'))
+port = 5000
 
 
 @app.route('/predict', methods=['POST'])
@@ -20,8 +21,9 @@ def get_prediction():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', port=port)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
