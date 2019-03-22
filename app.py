10bc963372023c53ac55ac866f64ee49a3644aef
Aakaash Jois <aakaashjois@gmail.com>
2019-03-22 10:19:20 -0400

    Update POST URL

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 CORS(app)
 model = load(open('./models/rf_v1.pkl', 'rb'))
-port = 5000
+port = int(os.environ.get('PORT', 5000))
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -23,5 +23,4 @@ def index():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
